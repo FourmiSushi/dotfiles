4a0c59490a77a653ae941a1cbd85610ac5dc19ed
FourmiSushi <neko@fourmisushi.soy>
2018-10-31 00:23:20 +0900

    ?

--- setfiles.sh
+++ setfiles.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-printf "password: "
-read password
-echo "$password" | sudo -S pacman -Syu
-echo "$password" | sudo -S pacman -S alacritty neovim yay-bin
-echo "$password" | sudo -S pacman -Rs variety
+read -sp "Password: " password
+echo "$password" | sudo -S pacman -S pacman-mirrorlist --noconfirm
+echo "$password" | sudo -S pacman -Syyu --noconfirm
+echo "$password" | sudo -S pacman -S alacritty neovim --noconfirm
+echo "$password" | sudo -S pacman -Rs variety --noconfirm
 echo "$password" | sudo -S pip install neovim
 
 mkdir -p $HOME/.config/nvim
 mkdir -p $HOME/.config/termite
-\cp -f ./nvim/ $HOME/.config/nvim/
-\cp -f ./termite/ $HOME/.config/termite
+\cp -rf ./nvim/ $HOME/.config/nvim/
+\cp -rf ./termite/ $HOME/.config/termite
 cd $HOME
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
