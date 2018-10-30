1afa157aeec6ca06046217383020443d7c47fdc3
FourmiSushi <neko@fourmisushi.soy>
2018-10-31 00:03:10 +0900

    added termite

--- setfiles.sh
+++ setfiles.sh
@@ -5,8 +5,11 @@ read password
 echo "$password" | sudo -S pacman -Syu
 echo "$password" | sudo -S pacman -S alacritty neovim yay-bin
 echo "$password" | sudo -S pip install neovim
-mkdir $HOME/.config/nvim
-cp ./nvim/ $HOME/.config/nvim/
+
+mkdir -p $HOME/.config/nvim
+mkdir -p $HOME/.config/termite
+\cp -f ./nvim/ $HOME/.config/nvim/
+\cp -f ./termite/ $HOME/.config/termite
 cd $HOME
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
