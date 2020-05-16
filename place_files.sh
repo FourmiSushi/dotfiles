a9a99701b74bf052853646da6e41374d54eb80d7
FourmiSushi <neko@fourmisushi.soy>
2020-05-16 14:08:57 +0900

    i3

--- place_files.sh
+++ place_files.sh
@@ -9,11 +9,17 @@ function clean_before_install() {
 	if [[ -e ~/.config/dunst/dunstrc ]] || [[ -L ~/.config/dunst/dunstrc ]]; then
 		rm -rf ~/.config/dunst/dunstrc
 	fi
-	if [[ -e ~/.xmobarrc ]] || [[ -L ~/.xmobarrc ]]; then
-		rm -rf ~/.xmobarrc
+	#if [[ -e ~/.xmobarrc ]] || [[ -L ~/.xmobarrc ]]; then
+	#	rm -rf ~/.xmobarrc
+	#fi
+	#if [[ -e ~/.xmonad/xmonad.hs ]] || [[ -L ~/.xmonad/xmonad.hs ]]; then
+	#	rm -rf ~/.xmonad/xmonad.hs
+	#fi
+	if [[ -e ~/.config/i3/config ]] || [[ -L ~/.config/i3/config ]]; then
+	rm -rf ~/.config/i3/config
 	fi
-	if [[ -e ~/.xmonad/xmonad.hs ]] || [[ -L ~/.xmonad/xmonad.hs ]]; then
-		rm -rf ~/.xmonad/xmonad.hs
+	if [[ -e ~/.config/i3status/config ]] || [[ -L ~/.config/i3status/config ]]; then
+	rm -rf ~/.config/i3status/config
 	fi
 }
 
@@ -24,8 +30,14 @@ function make_dir_if_not_exists() {
 	if [[ ! -d ~/.config/dunst ]]; then
 		mkdir -p ~/.config/dunst
 	fi
-	if [[ ! -d ~/.xmonad ]]; then
-		mkdir -p ~/.xmonad
+	#if [[ ! -d ~/.xmonad ]]; then
+	#	mkdir -p ~/.xmonad
+	#fi
+	if [[ ! -d ~/.config/i3 ]]; then
+	mkdir -p ~/.config/i3
+	fi
+	if [[ ! -d ~/.config/i3status ]]; then
+	mkdir -p ~/.config/i3status
 	fi
 }
 
@@ -38,5 +50,7 @@ make_dir_if_not_exists
 
 ln -s $script_dir/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -s $script_dir/dunstrc ~/.config/dunst/dunstrc
-ln -s $script_dir/xmobarrc ~/.xmobarrc
-ln -s $script_dir/xmonad.hs ~/.xmonad/xmonad.hs
+# ln -s $script_dir/xmobarrc ~/.xmobarrc
+# ln -s $script_dir/xmonad.hs ~/.xmonad/xmonad.hs
+ln -s $script_dir/i3/config ~/.config/i3/config
+ln -s $script_dir/i3status/config ~/.config/i3status/config
