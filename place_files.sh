1510e1fc5a7fcf696f7a65ff5bd74bbd86ad0d20
FourmiSushi <neko@fourmisushi.soy>
2021-07-12 15:45:53 +0900

    無限シンボリックリンク対策

--- place_files.sh
+++ place_files.sh
@@ -35,16 +35,31 @@ function clean_configs() {
 	fi
 }
 
+function place_common_config() {
+	ln -s $script_dir/alacritty ~/.config/alacritty
+	ln -s $script_dir/dunst ~/.config/dunst
+	ln -s $script_dir/profile ~/.profile
+	ln -s $script_dir/gitconfig ~/.gitconfig
+
+}
+
 function place_xmonad_config() {
 	ln -s $script_dir/xmobarrc ~/.xmobarrc
 	ln -s $script_dir/xmonad ~/.xmonad
+	place_common_config
 }
 
 function place_i3_config() {
 	ln -s $script_dir/i3 ~/.config/i3
 	ln -s $script_dir/i3status ~/.config/i3status
 	ln -s $script_dir/i3blocks ~/.config/i3blocks
+	place_common_config
 }
+
+whiptail --yesno "このスクリプトを実行することにより、このスクリプトと衝突する既存の設定ファイルが破壊されます。続けますか？" 20 60
+if [ "$?" = 1 ]; then
+	exit
+fi
 
 pacman -Qqe yay-bin >/dev/null
 if [ "$?" = 1 ]; then
@@ -59,19 +74,10 @@ if [ "$?" = 0 ]; then
 	sh $script_dir/install_pkgs.sh
 fi
 
-whiptail --yesno "すでにある設定ファイルを削除しますか？" 20 60
-if [ "$?" = 0 ]; then
-	clean_configs
-fi
+clean_configs
 
 choice=$(whiptail --menu "どの構成を使いますか？" 0 0 10 0 "i3+i3blocks" 1 "xmonad+xmobar" 3>&1 1>&2 2>&3)
 case "$choice" in
 0) place_i3_config ;;
 1) place_xmonad_config ;;
-*) place_i3_config ;;
 esac
-
-ln -s $script_dir/alacritty ~/.config/alacritty
-ln -s $script_dir/dunst ~/.config/dunst
-ln -s $script_dir/profile ~/.profile
-ln -s $script_dir/gitconfig ~/.gitconfig
