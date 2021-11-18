136bc6741c2c57ed2b94f485168f2d850f94c6f9
FourmiSushi <neko@fourmisushi.soy>
2021-11-18 11:05:52 +0900

    add code

--- after_place.sh
+++ after_place.sh
@@ -8,6 +8,6 @@ script_dir=$(
 
 ssh-keygen -t ed25519
 gh auth login
-.$script_dir/install_mplus1mn.sh
+$script_dir/install_mplus1mn.sh
 sudo cp $script_dir/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
 localectl set-x11-keymap jp
